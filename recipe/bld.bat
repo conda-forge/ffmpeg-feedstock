3d89bddbeca37f22cd8a832bd66a880956a22d1d
Casper van der Wel <caspervdw@gmail.com>
2017-01-24 14:56:29 +0100

    Include windows headers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,16 @@
-set FFMPEG_FN=ffmpeg-2.8.6-win%ARCH%-shared
+set FFMPEG_FN=ffmpeg-%PKG_VERSION%-win%ARCH%
 
-7za x %SRC_DIR%\%FFMPEG_FN%.7z -o%SRC_DIR%
-mkdir %SCRIPTS%
-copy %SRC_DIR%\%FFMPEG_FN%\bin %SCRIPTS%
-copy %SRC_DIR%\%FFMPEG_FN%\licenses %SCRIPTS%
-copy %SRC_DIR%\%FFMPEG_FN%\README.txt %SCRIPTS%
-copy %SRC_DIR%\%FFMPEG_FN%\ff-prompt.bat %SCRIPTS%
+Invoke-WebRequest "https://ffmpeg.zeranoe.com/builds/win%ARCH%/dev/%FFMPEG_FN%-dev.7z" -OutFile "%SRC_DIR%\%FFMPEG_FN%-dev.7z"
+
+7za x %SRC_DIR%\%FFMPEG_FN%-shared.7z -o%SRC_DIR%
+7za x %SRC_DIR%\%FFMPEG_FN%-dev.7z -o%SRC_DIR%
+
+copy %SRC_DIR%\%FFMPEG_FN%-shared\bin %LIBRARY_BIN%
+copy %SRC_DIR%\%FFMPEG_FN%-dev\include %LIBRARY_INC%
+copy %SRC_DIR%\%FFMPEG_FN%-dev\lib %LIBRARY_LIB%
+copy %SRC_DIR%\%FFMPEG_FN%-shared\README.txt %RECIPE_DIR%
+
+mkdir %RECIPE_DIR%\licenses
+copy %SRC_DIR%\%FFMPEG_FN%-shared\lincenses %RECIPE_DIR%\licenses
+
+exit 0
