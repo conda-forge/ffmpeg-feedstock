a69c1de9eadd67021af10966de7d5fb576b6b66e
Casper van der Wel <caspervdw@gmail.com>
2017-01-25 09:06:16 +0100

    Use robocopy to copy subdirs too

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,15 @@ curl -L -O "https://ffmpeg.zeranoe.com/builds/win%ARCH%/dev/%FFMPEG_FN%-dev.7z"
 7za x %SRC_DIR%\%FFMPEG_FN%-shared.7z -o%SRC_DIR%
 7za x %FFMPEG_FN%-dev.7z -o%SRC_DIR%
 
-copy %SRC_DIR%\%FFMPEG_FN%-shared\bin %LIBRARY_BIN%
-copy %SRC_DIR%\%FFMPEG_FN%-dev\include %LIBRARY_INC%
-copy %SRC_DIR%\%FFMPEG_FN%-dev\lib %LIBRARY_LIB%
+robocopy %SRC_DIR%\%FFMPEG_FN%-shared\bin\ %LIBRARY_BIN%\ *.* /E
+if %ERRORLEVEL% GEQ 8 exit 1
+
+robocopy %SRC_DIR%\%FFMPEG_FN%-dev\include\ %LIBRARY_INC%\ *.* /E
+if %ERRORLEVEL% GEQ 8 exit 1
+
+robocopy %SRC_DIR%\%FFMPEG_FN%-dev\lib\ %LIBRARY_LIB%\ *.* /E
+if %ERRORLEVEL% GEQ 8 exit 1
+
 copy %SRC_DIR%\%FFMPEG_FN%-shared\README.txt %RECIPE_DIR%
 
 mkdir %RECIPE_DIR%\licenses
