b5ba4b6d8c85406a6c3dfabb916f81997599c095
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-12-28 09:33:27 -0500

    Add CC to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,24 +5,22 @@ unset SUBDIR
 
 ./configure \
         --prefix="${PREFIX}" \
+        --cc=${CC} \
         --disable-doc \
         --disable-openssl \
-        --enable-shared \
-        --enable-static \
-        --extra-cflags="-Wall -g -m64 -pipe -O3 -march=x86-64 -fPIC" \
-        --extra-cxxflags="-Wall -g -m64 -pipe -O3 -march=x86-64 -fPIC" \
-        --extra-libs="-lpthread -lm -lz" \
-        --enable-zlib \
+        --enable-avresample \
+        --enable-gnutls \
+        --enable-gpl \
+        --enable-hardcoded-tables \
+        --enable-libfreetype \
+        --enable-libopenh264 \
+        --enable-libx264 \
         --enable-pic \
         --enable-pthreads \
-        --enable-gpl \
+        --enable-shared \
+        --enable-static \
         --enable-version3 \
-        --enable-hardcoded-tables \
-        --enable-avresample \
-        --enable-libfreetype \
-        --enable-gnutls \
-        --enable-libx264 \
-        --enable-libopenh264
+        --enable-zlib
 
 make -j${CPU_COUNT}
 make install -j${CPU_COUNT}
