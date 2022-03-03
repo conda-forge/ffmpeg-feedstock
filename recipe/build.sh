f06dc3646056a41f333a9b4fbae21a314a1091f0
Mark Harfouche <mark.harfouche@gmail.com>
2022-03-03 08:34:19 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,6 +32,7 @@ if [[ "${target_platform}" == "linux-64" ]]; then
   extra_args="${extra_args} --enable-libopenh264"
   extra_args="${extra_args} --enable-libmp3lame"
   extra_args="${extra_args} --enable-libsvtav1"
+  extra_args="${extra_args} --enable-libvpx"
 elif [[ "${target_platform}" == osx-* ]]; then
   if [[ "${target_platform}" == osx-arm64 ]]; then
     extra_args="--enable-neon"
@@ -45,6 +46,7 @@ elif [[ "${target_platform}" == osx-* ]]; then
   extra_args="${extra_args} --enable-libopenh264"
   extra_args="${extra_args} --enable-libmp3lame"
   extra_args="${extra_args} --enable-libsvtav1"
+  extra_args="${extra_args} --enable-libvpx"
   # See https://github.com/conda-forge/ffmpeg-feedstock/pull/115
   # why this flag needs to be removed.
   sed -i.bak s/-Wl,-single_module// configure
@@ -62,7 +64,6 @@ fi
         ${extra_args} \
         --enable-libx264 \
         --enable-libxml2 \
-        --enable-libvpx \
         --enable-pic \
         --enable-pthreads \
         --enable-shared \
