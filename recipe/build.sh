1cfd05a8c427f07d9de4c52d4ec88bccb7057a72
Mark Harfouche <mark.harfouche@gmail.com>
2022-05-30 15:39:48 -0400

    Add a configuration for lgpl

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,18 +23,25 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
   EXTRA_CONFIGURE_OPTIONS="--enable-cross-compile --arch=$ARCH --target-os=$OS --cross-prefix=$HOST- --host-cc=$CC_FOR_BUILD"
 fi
 
+extra_args=""
 if [[ "${target_platform}" == "linux-64" ]]; then
   extra_args=--enable-vaapi
 elif [[ "${target_platform}" == osx-* ]]; then
   if [[ "${target_platform}" == osx-arm64 ]]; then
-    extra_args=" --enable-neon"
+    extra_args="${extra_args} --enable-neon"
   else
-    extra_args="--disable-videotoolbox"
+    extra_args="${extra_args} --disable-videotoolbox"
   fi
 
   # See https://github.com/conda-forge/ffmpeg-feedstock/pull/115
   # why this flag needs to be removed.
   sed -i.bak s/-Wl,-single_module// configure
+fi
+
+if [[ "${license_family}" == "gpl" ]]; then
+    extra_args="${extra_args} --enable-gpl --enable-libx264 --enable-libx265"
+else
+    extra_args="${extra_args} --disable-gpl"
 fi
 
 ./configure \
@@ -44,13 +51,10 @@ fi
         --disable-openssl \
         --enable-demuxer=dash \
         --enable-gnutls \
-        --enable-gpl \
         --enable-hardcoded-tables \
         --enable-libfreetype \
         --enable-libopenh264 \
         ${extra_args} \
-        --enable-libx264 \
-        --enable-libx265 \
         --enable-libaom \
         --enable-libsvtav1 \
         --enable-libxml2 \
