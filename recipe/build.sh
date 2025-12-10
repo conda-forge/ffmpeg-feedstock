5e6db0aa6a5e07f85cba8bfcc7525cbffe53d6b3
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-10 09:35:15 -0500

    Remove windres to prevent conflicts in build
    Remove windres if found to avoid conflicts.

--- recipe/build.sh
+++ recipe/build.sh
@@ -59,7 +59,8 @@ if [[ "${target_platform}" == "win-64" ]]; then
   # it seems that gnu-windres that it is found
   # but we want to use vc's rc compiler
   # https://github.com/conda-forge/ffmpeg-feedstock/pull/351#issuecomment-3637099344
-  extra_args="${extra_args} --disable-gnu-windres"
+  which windres
+  rm -f $(which windres)
 
   # Delete line that includes unistd.h from zconf. we should patch this
   # better for LLVM compatibility
